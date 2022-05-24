485facec75f9fb6e9bf7249975887eaedae3d01a
geekOgeek <arjun.sisodia8@gmail.com>
2022-05-24 20:21:26 +0530

    attend

--- src/app/features/dashboard/components/attendance/attendance.component.ts
+++ src/app/features/dashboard/components/attendance/attendance.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { CalendarOptions } from '@fullcalendar/angular';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-attendance',
   templateUrl: './attendance.component.html',
@@ -22,14 +23,15 @@ export class AttendanceComponent {
     selectMirror: true,
     dayMaxEvents: true
   };
-  constructor(private router: Router, private httpClient: HttpClient) {}
+  profiledata!: any;
+
+  constructor( private http: HttpClient, private router: Router, private httpClient: HttpClient) {}
   onDateClick(res: any) {
-    alert('Clicked on date : ' + res.dateStr);
+    console.log('Clicked on date : ' + res.dateStr);
   }
   ngOnInit() {
-    setTimeout(() => {
-      this.Events.push({ title: "Attendance marked", start: "2022-05-22" });
-    }, 2200);
+    this.profiledata = JSON.parse(localStorage.getItem('profileabworker') || '{}');
+    this.attendanceListByWorker(this.profiledata?.id)
     setTimeout(() => {
       this.calendarOptions = {
         initialView: 'dayGridMonth',
@@ -37,6 +39,22 @@ export class AttendanceComponent {
         events: this.Events,
       };
     }, 2500);
+  }
+
+  attendanceListByWorker(workerId: any = '') {
+    if(!workerId) return;
+    const endpoint = `https://cors-everywhere.herokuapp.com/http://abprojectsserver-env.eba-5pjjn569.us-east-1.elasticbeanstalk.com/attendanceByUser?userId=${workerId}`;
+    const headers = {headers: new HttpHeaders({ "Content-type": "application/json", "Authorization": localStorage.getItem("abprojectsToken") || '' })}
+
+    this.http.get(endpoint, headers)
+    .subscribe((res: any): void => {
+      setTimeout(() => {
+        res.result.forEach((obj: any) => {
+          this.Events.push({ start: obj.attendanceDate ? obj.attendanceDate.split("T")[0]: '', title: obj.title })
+        });
+        console.log(this.Events, "his.Events")
+      }, 2200);
+    })
   }
 
   goto(route: string = '') {
