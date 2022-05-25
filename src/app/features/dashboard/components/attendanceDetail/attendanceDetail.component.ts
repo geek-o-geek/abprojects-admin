79e3589441e9b780870602e067dc1a85c26fe578
geekOgeek <arjun.sisodia8@gmail.com>
2022-05-25 18:30:54 +0530

    loca

--- src/app/features/dashboard/components/attendanceDetail/attendanceDetail.component.ts
+++ src/app/features/dashboard/components/attendanceDetail/attendanceDetail.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { CalendarOptions } from '@fullcalendar/angular';
 import { Router } from '@angular/router';
 
 @Component({
@@ -12,48 +10,18 @@ export class AttendanceDetailComponent {
   lat!: number;
   lng!: number;
   Events: any[] = [];
-  calendarOptions: CalendarOptions = {
-    headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-    },
-    initialView: 'dayGridMonth',
-    weekends: true,
-    editable: true,
-    selectable: true,
-    selectMirror: true,
-    dayMaxEvents: true
-  };
   attendanceDetailData!: any;
-
-  constructor( private http: HttpClient, private router: Router, private httpClient: HttpClient) {}
-  
   myDetails: any = [];
-  mylat: string = "51.076101702905575"; // default lat
-  mylong: string = "10.867780927493548"; // default long
-  zoom = 12;
-  initialCoordinates = {
-    lat: '',
-    lng: '',
-  };
-  mapConfigurations = {
-    disableDefaultūI: true,
-    fullscreenControl: true,
-    zoomControl: true,
-    mapTypeId: "roadmap",
-    scrollwheel: false,
-    disableDoubleClickZoom: true,
-  };
+
+  constructor(private router: Router) {}
+ 
   ngOnInit() {
     this.attendanceDetailData = JSON.parse(localStorage.getItem("resAttendanceDetail") || '{}');
   }
 
-
   ngAfterViewInit(){
-    this.lat = parseInt(this.attendanceDetailData?.startLocation);
-    this.lng = parseInt(this.attendanceDetailData?.endLocation);
-    console.log(this.attendanceDetailData, "this.attendanceDetailData")
+    this.lat = +this.attendanceDetailData?.startLocation;
+    this.lng = +this.attendanceDetailData?.endLocation;
   }
 
   goto(route: string = '') {
