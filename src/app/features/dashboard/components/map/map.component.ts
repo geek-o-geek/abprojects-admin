01f0cf706a03122d2e1ca601612ad8f3d10f3a46
geekOgeek <arjun.sisodia8@gmail.com>
2022-06-13 23:20:40 +0530

    code pushed

--- src/app/features/dashboard/components/map/map.component.ts
+++ src/app/features/dashboard/components/map/map.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -12,12 +13,43 @@ export class MapComponent implements OnInit {
   lng!: number;
   Events: any[] = [];
   attendanceDetailData: any = [];
+  attendanceDetailDataBackup: any = [];
   myDetails: any = [];
   dt: string = '';
+  form!: FormGroup;
+  wards: any[] = []
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient,
+    private fb: FormBuilder,
+    private router: Router) {
+    this.createForm();
+  }
   
-  ngOnInit() {}
+  ngOnInit() {
+    this.form.valueChanges
+    .subscribe((item: any) => {
+      const { wardId, dt } = item;
+
+      this.attendanceDetailData = this.attendanceDetailDataBackup.filter((item: any): any => {
+        const attendDate = item?.attendanceDate.split("T")[0]
+     
+        if(dt === attendDate){ 
+          return item;
+        }
+      })
+
+      console.log(this.attendanceDetailData, "this.attendanceDetailData")
+    })
+
+    const endpoint = "https://cors-everywhere.herokuapp.com/http://abprojectsserver-env.eba-5pjjn569.us-east-1.elasticbeanstalk.com/get/wards";
+    const headers = {headers: new HttpHeaders({ "Content-type": "application/json", "Authorization": localStorage.getItem("abprojectsToken") || '' })}
+
+    this.http.get(endpoint, headers)
+    .subscribe((res: any) => {
+      this.wards = res?.data || []
+    })
+  }
+
   ngAfterViewInit() {
     this.dt = new Date().toISOString().split("T")[0];
   
@@ -27,6 +59,7 @@ export class MapComponent implements OnInit {
       this.http.get(endpoint, headers)
       .subscribe((res: any): void => {
         this.attendanceDetailData = res?.result || [];
+        this.attendanceDetailDataBackup = res?.result || [];
         this.updateLocation();
       });
   }
@@ -38,6 +71,14 @@ export class MapComponent implements OnInit {
 
   goto(route: string = '') {
     this.router.navigateByUrl(route)
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      wardId: [''],
+      wardFilter: [''],
+      dt: ['']
+    })
   }
 
   logout() {
