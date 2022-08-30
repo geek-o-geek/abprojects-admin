fc200702d081023434f7f3c8bbd31c1e1822ec6e
geekOgeek <arjun.sisodia8@gmail.com>
2022-08-30 09:45:09 +0530

    fix

--- src/app/features/dashboard/components/graphs/graphs.component.ts
+++ src/app/features/dashboard/components/graphs/graphs.component.ts
@@ -13,6 +13,7 @@ export class GraphsComponent {
   single: any[] = []
   view: [number, number] = [1150, 400];
   ageResult: any[] = []
+  wardsResult: any [] = []
 
   // options
   gradient: boolean = true;
@@ -22,7 +23,16 @@ export class GraphsComponent {
   legendPosition: any = 'below';
 
   colorScheme: any = {
-    domain: ['#5AA454', '#A10A28']
+    domain: ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+    '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+    '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+    '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+    '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+    '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+    '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+    '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+    '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+    '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF']
   };
 
   // options
@@ -54,9 +64,11 @@ export class GraphsComponent {
      ]
 
      this.ageResult = data?.ageDemographic || []
+     this.wardsResult = data?.wardsDemographic || []
 
      Object.assign(this, { single: this.single });
      Object.assign(this, { ageResult: this.ageResult });
+     Object.assign(this, { wardsResult: this.wardsResult });
     })
   }
 
